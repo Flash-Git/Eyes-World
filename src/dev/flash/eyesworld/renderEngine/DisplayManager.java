83938d5710e8ca467741143e66ad4842546bff8c
Flash <Flash.Jaq@gmail.com>
2016-12-29 07:10:59 +0000

    Switched to lwjgl 2

---
+++ src/dev/flash/eyesworld/renderEngine/DisplayManager.java
@@ -0,0 +1,24 @@
+package dev.flash.eyesworld.renderEngine;
+
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+/**
+ * Created by Flash on 28/12/2016.
+ */
+
+public class DisplayManager {
+
+    public static void createDisplay(){
+        Display.setDisplayMode(new DisplayMode());
+    }
+
+    public static void updateDisplay(){
+
+    }
+
+    public static void closeDisplay(){
+
+    }
+    */
+}
