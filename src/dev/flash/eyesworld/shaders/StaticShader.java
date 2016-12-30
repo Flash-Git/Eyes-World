dc07f8dfc99ce2248d4da0c962ca0634339705e9
Flash <Flash.Jaq@gmail.com>
2016-12-30 14:28:51 +0000

    Adding transformation matrix

--- src/dev/flash/eyesworld/shaders/StaticShader.java
+++ src/dev/flash/eyesworld/shaders/StaticShader.java
@@ -1,5 +1,7 @@
 package dev.flash.eyesworld.shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 /**
  * Created by Flash on 30/12/2016.
  */
@@ -7,6 +9,8 @@ public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/dev/flash/eyesworld/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/dev/flash/eyesworld/shaders/fragmentShader.txt";
+
+    private int location_transformationMatrix;
 
     public StaticShader(){
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -16,6 +20,15 @@ public class StaticShader extends ShaderProgram {
     protected void bindAttributes(){
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    public void loadTransformationMatrix(Matrix4f matrix){
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
 
 }
