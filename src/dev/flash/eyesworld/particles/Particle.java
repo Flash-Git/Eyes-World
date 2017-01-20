03c9f785a32ac70218193aa6e9d5e187706cf300
Flash <flash.jaq@gmail.com>
2017-01-20 23:48:39 +0000

    Created initial particle classes

---
+++ src/dev/flash/eyesworld/particles/Particle.java
@@ -0,0 +1,9 @@
+package dev.flash.eyesworld.particles;
+
+/**
+ * Created by Flash on 20/01/2017.
+ */
+
+public class Particle {
+	
+}
---
+++ src/dev/flash/eyesworld/particles/ParticleShader.java
@@ -0,0 +1,42 @@
+package dev.flash.eyesworld.particles;
+
+import org.lwjgl.util.vector.Matrix4f;
+
+import dev.flash.eyesworld.shaders.ShaderProgram;
+
+/**
+ * Created by Flash on 20/01/2017.
+ */
+
+public class ParticleShader extends ShaderProgram {
+	
+	private static final String VERTEX_FILE = "src/dev/flash/eyesworld/particles/particleVShader.txt";
+	private static final String FRAGMENT_FILE = "src/dev/flash/eyesworld/particleFShader.txt";
+	
+	private int location_modelViewMatrix;
+	private int location_projectionMatrix;
+	
+	public ParticleShader() {
+		super(VERTEX_FILE, FRAGMENT_FILE);
+	}
+	
+	@Override
+	protected void getAllUniformLocations() {
+		location_modelViewMatrix = super.getUniformLocation("modelViewMatrix");
+		location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+	}
+	
+	@Override
+	protected void bindAttributes() {
+		super.bindAttribute(0, "position");
+	}
+	
+	protected void loadModelViewMatrix(Matrix4f modelView) {
+		super.loadMatrix(location_modelViewMatrix, modelView);
+	}
+	
+	protected void loadProjectionMatrix(Matrix4f projectionMatrix) {
+		super.loadMatrix(location_projectionMatrix, projectionMatrix);
+	}
+	
+}
